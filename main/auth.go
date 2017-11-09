9f6de0406445fcd8d3daa63f4ab5e4051b7349fa
Jaume Capdevila <contact@jaumecapdevila>
2017-11-09 14:51:17 +0100

    Moved all the chat components to the main folder

---
+++ main/auth.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+	if err != nil {
+		// Some other error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// success - call the next handler
+	h.next.ServeHTTP(w, r)
+}
+
+// MustAuth returns a new authHandler object
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
