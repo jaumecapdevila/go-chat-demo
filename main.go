ce7d0a3d9ab5b7d65ee10f20e809b7007e02c8ea
Jaume Capdevila <contact@jaumecapdevila>
2017-11-10 15:21:37 +0100

    Added logrus to handle all the logs of the application

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"sync"
 	"text/template"
 
 	"github.com/jaumecapdevila/chat/auth"
-	"github.com/jaumecapdevila/chat/trace"
+	"github.com/jaumecapdevila/chat/config"
+	log "github.com/sirupsen/logrus"
 )
 
 type templateHandler struct {
@@ -26,8 +29,33 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, r)
 }
 
+func init() {
+	config := config.Load()
+	if _, err := os.Stat(config.Logger.Dir); os.IsNotExist(err) {
+		err := os.MkdirAll(config.Logger.Dir, 0755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	if _, err := os.Stat(path.Join(config.Logger.Dir, "dev.log")); os.IsNotExist(err) {
+		_, err := os.Create(path.Join(config.Logger.Dir, "dev.log"))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	f, err := os.OpenFile(path.Join(config.Logger.Dir, "dev.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(f)
+}
+
 func main() {
-	tracer := trace.Log()
 	var port = flag.String("port", ":8080", "The port of the application.")
 	flag.Parse()
 	r := newRoom()
@@ -37,13 +65,12 @@ func main() {
 	// we dont need an object because this function doesn't need to store any state
 	http.HandleFunc("/auth/", auth.LoginHandler)
 	http.Handle("/room", r)
-
 	// Run the chat in other goroutine
 	// in order to let the main goroutine to run the webserver
 	go r.run()
 	// Start the web server
 	if err := http.ListenAndServe(*port, nil); err != nil {
-		tracer.Trace("ListenAndServe:", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
--- trace/tracer.go
+++ trace/tracer.go
@@ -3,11 +3,6 @@ package trace
 import (
 	"fmt"
 	"io"
-	"log"
-	"os"
-	"path"
-
-	"github.com/jaumecapdevila/chat/config"
 )
 
 // Tracer interface
@@ -19,17 +14,11 @@ type tracer struct {
 	out io.Writer
 }
 
-type logTracer struct{}
-
 type nilTracer struct{}
 
 func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
-}
-
-func (t *logTracer) Trace(a ...interface{}) {
-	log.Print(a...)
 }
 
 func (t *nilTracer) Trace(a ...interface{}) {}
@@ -39,38 +28,7 @@ func New(w io.Writer) Tracer {
 	return &tracer{out: w}
 }
 
-// Log returns a Log Tracer object
-func Log() Tracer {
-	return &logTracer{}
-}
-
 // Off returns a silent tracer
 func Off() Tracer {
 	return &nilTracer{}
-}
-
-func init() {
-	config := config.Load()
-	if _, err := os.Stat(config.Logger.Dir); os.IsNotExist(err) {
-		err := os.MkdirAll(config.Logger.Dir, 0755)
-		if err != nil {
-			fmt.Printf("Error %v", err)
-			os.Exit(1)
-		}
-	}
-	if _, err := os.Stat(path.Join(config.Logger.Dir, "error.log")); os.IsNotExist(err) {
-		nf, err := os.Create(path.Join(config.Logger.Dir, "error.log"))
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		log.SetOutput(nf)
-		return
-	}
-	f, err := os.OpenFile(path.Join(config.Logger.Dir, "error.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	log.SetOutput(f)
 }
